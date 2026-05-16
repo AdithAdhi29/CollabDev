91aa7dea6cd19ba2b6a3f5725334d27e101a12b8
AdithAdi29 <adithinbox@gmail.com>
2026-05-16 18:08:49 +0530

    Tested the routr handler with next and response send cases

--- src/app.js
+++ src/app.js
@@ -2,20 +2,31 @@ const express = require("express");
 
 const app = express();
 
-app.get("/test/:UserID/:Name", (req, res) => {
-  console.log(req.query);
-  console.log(req.params);
-  res.send("Testing page loaded....");
-});
+// app.get("/test/:UserID/:Name", (req, res) => {
+//   console.log(req.query);
+//   console.log(req.params);
+//   res.send("Testing page loaded....");
+// });
 
-app.post("/test", (req, res) => {
-  console.log("Updated the Database...");
-  res.send("Successfully updated the data base.");
-});
+//app.use("/test", [rh1, rh2, rh3, rh4, rh5, rh6]);
 
-app.use((req, res) => {
-  res.send("Hello world..");
-});
+app.use(
+  "/test",
+  (req, res, next) => {
+    console.log("Test 1");
+    //res.send("Test 1 completed successfully..");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Test 2");
+    next();
+    res.send("Test 2 completed successfully..");
+  },
+  (req, res, next) => {
+    console.log("Test 3");
+    res.send("Test 3 completed successfully..");
+  }
+);
 
 app.listen(7777, () => {
   console.log("Server running sucessfully...");
